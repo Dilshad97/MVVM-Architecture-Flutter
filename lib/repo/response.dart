7cb02a9b856513351554ff01a0244e3e73b15776
kishan_vekariya <kvekariya@tibicle.com>
2021-12-02 11:39:59 +0530

    MVVM architecture

--- lib/repo/response.dart
+++ lib/repo/response.dart
@@ -15,3 +15,18 @@ class Webservice {
     }
   }
 }
+
+
+// class Webservices {
+//   Future<List<Movie>> fetchMovies(String keyword) async {
+//     final url = "http://www.omdbapi.com/?s=$keyword&apikey=eb0d5538";
+//     final response = await http.get(url);
+//     if (response.statusCode == 200) {
+//       final body = jsonDecode(response.body);
+//       final Iterable json = body["Search"];
+//       return json.map((movie) => Movie.fromJson(movie)).toList();
+//     } else {
+//       throw Exception("Unable to perform request!");
+//     }
+//   }
+// }
